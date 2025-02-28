ec9ecf4e128d039cb75ab925096ebeaf005c0b37
Fredy Hernández <fhernandez-2023176@kinal.edu.gt>
2025-02-28 17:47:59 -0600

    Se hizo un avanze de la funcion listar empresas

--- src/company/company.controller.js
+++ src/company/company.controller.js
@@ -25,3 +25,76 @@ export const saveCompany = async (req, res) => {
         })
     }
 }
+
+export const getCompany = async (req, res) => {
+    try{
+        const { limite = 5, desde = 0} = req.body
+        const query = { status : true}
+
+        const [total, company ] = await Promise.all([
+            Company.countDocuments(query),
+            Company.find(query)
+                   .skip(Number(desde))
+                   .limit(Number(limite))
+        ])
+
+        return res.status(200).json({
+            success: true,
+            total,
+            company
+        })
+    }catch(err){
+        return res.status(500).json({
+            success: false,
+            message: "Error al lsitar las empresas",
+            error: err.message
+        });
+    }
+}
+
+export const getCompanyList = async (rea, res) => {
+    try{
+        const { limit = 10, from = 0 } = req.query;
+        const { listarOrden } = req.body;
+        const query = { status: true };
+
+        let ordenOptions = {};
+
+        switch(listarOrden) {
+            case "yearsOfExperience":
+                ordenOptions = { yearsOfExperience: -1}
+                break
+            case "companyCategory":
+                ordenOptions = { companyCategory: 1}
+                break
+            case "A-Z":
+                ordenOptions = { companyName: 1 }
+                break
+            case "Z-A":
+                ordenOptions = { companyName: -1}
+                break 
+            default:
+                ordenOptions = { createAt: -1}
+        }
+
+        const [total, company] = await Promise.all([
+            Company.countDocuments(query),
+            Company.find(query)
+                   .sort(ordenOptions)
+                   .skip(Number(from))
+                   .limit(Number(limit))
+        ])
+
+        return res.status(200).json({
+            success: true,
+            total,
+            company
+        })
+    }catch(err){
+        return res.status(500).json({
+            success: false,
+            message: "Error al listar las empresas",
+            error: err.message
+        })
+    }
+}
--- src/company/company.routes.js
+++ src/company/company.routes.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
 import { addCompanyValidators } from "../middlewares/company-validators.js";
-import { saveCompany } from "./company.controller.js";
+import { saveCompany, getCompany, getCompanyList } from "./company.controller.js";
 
 const router = Router();
 
 router.post("/addCompany", addCompanyValidators, saveCompany)
+
+router.get("/", getCompany)
+
+router.get("/list", getCompanyList)
 
 export default router;
